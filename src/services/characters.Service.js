02f963db63617d1f56dfb394d74298c338be7625
Thiago Lima <102415139+Thyago-limma@users.noreply.github.com>
2022-07-11 22:03:03 -0300

    Update characters.Service.js

--- src/services/characters.Service.js
+++ src/services/characters.Service.js
@@ -1,31 +1,31 @@
 const characters = require('../models/Characters');
 
 const readAllCharactersUrl = async (req, res) => {
-    const Characters = await characters.find();
-    return Characters;
+  const Characters = await characters.find();
+  return Characters;
 };
 
 const readAllCharactersByIdUrl = async (id) => {
-    const AllCharacters = characters.findById (id);
-    return await AllCharacters;
+  const AllCharacters = characters.findById(id);
+  return await AllCharacters;
 };
 
 const createCharacterUrl = async (newCharacter) => {
-    const createdCharacter = await characters.create (newCharacter);
-    return createdCharacter; 
+  const createdCharacter = await characters.create(newCharacter);
+  return createdCharacter;
 };
 
-const updateCharacterUrl = async (id, editCharacter) => await characters.find(id, editCharacter).setOptions({
-    returnOriginal: false
-});
+const updateCharacterUrl = async (id, editCharacter) =>
+  await characters.find(id, editCharacter).setOptions({
+    returnOriginal: false,
+  });
 
 const deleteCharacterUrl = async (id) => await characters.findByIdAndDelete(id);
 
-
 module.exports = {
-    readAllCharactersUrl,
-    readAllCharactersByIdUrl,
-    createCharacterUrl,
-    updateCharacterUrl,
-    deleteCharacterUrl,
+  readAllCharactersUrl,
+  readAllCharactersByIdUrl,
+  createCharacterUrl,
+  updateCharacterUrl,
+  deleteCharacterUrl,
 };
