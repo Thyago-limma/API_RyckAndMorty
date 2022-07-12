2010e3bafe42179ed140dadc2a7dfe13933d4c74
Thiago Lima <102415139+Thyago-limma@users.noreply.github.com>
2022-07-11 22:00:51 -0300

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/rickyAndMorty-db'
+const uri = 'mongodb://localhost:27017/rickyAndMorty-db';
 
 function connectToDatabase() {
   mongoose.connect(uri).then(() => {
@@ -9,12 +9,11 @@ function connectToDatabase() {
         useUnifiedTopology: true, //
       })
 
-      .then(() => 
-        console.log('MongoDb conectado com sucesso!!')) //quando conectar
+      .then(() => console.log('MongoDb conectado com sucesso!!')) //quando conectar
       .catch(
         (error) => console.log(`error ao conectar com MongoDb, erro: ${error}`), //erro caso não conectar
       );
   });
-};
+}
 
-module.exports = connectToDatabase ;
+module.exports = connectToDatabase;
